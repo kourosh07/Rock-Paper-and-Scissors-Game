5d2d1902d2a27e8d65b88f04b105c02c20221d0b
Kourosh Rahimi <kourosh2004rahimi@gmail.com>
2024-10-09 20:55:06 +0330

    Add files via upload

--- app.py
+++ app.py
@@ -13,25 +13,38 @@ while True:
         if computer == "Paper":
             print("You lose!", computer, "covers", player)
             cpu_score+=1
+            computer = random.choice(choices)
+
         else:
             print("You win!", player, "smashes", computer)
             player_score+=1
+            computer = random.choice(choices)
+
     elif player == "Paper":
         if computer == "Scissors":
             print("You lose!", computer, "cut", player)
             cpu_score+=1
+            computer = random.choice(choices)
+
         else:
             print("You win!", player, "covers", computer)
             player_score+=1
+            computer = random.choice(choices)
+
     elif player == "Scissors":
         if computer == "Rock":
             print("You lose...", computer, "smashes", player)
             cpu_score+=1
+            computer = random.choice(choices)
+
         else:
             print("You win!", player, "cut", computer)
             player_score+=1
+            computer = random.choice(choices)
+
     elif player=='End':
         print("Final Scores:")
         print(f"CPU:{cpu_score}")
         print(f"Player:{player_score}")
         break
+
